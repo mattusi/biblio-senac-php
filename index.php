26e7d5f0178f04eebba92799d283706e6d08e36b
Matheus Santos <MatheusTusi@Matheus-Mac-mini.local>
2017-04-30 16:19:40 -0300

    add SQL commit

--- index.php
+++ index.php
@@ -3,6 +3,24 @@
 		<title>Teste do PHP no Azure</title> 
 	</head>
 	<body> 
-		<?php echo '<p>Hello World !!</p>'; ?>
+		<?php echo '<p>Hello World !!</p>';
+		//SQL database information
+		$host = "tcp:bibliodb.database.windows.net,1433";
+		$user = "mattusi@bibliodb.database.windows.net";
+		$pwd = "12345678Casa";
+		$db = "bibliodb";
+ 
+		try {
+    		$conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+    		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+ 
+    		$sqlQuery = $conn->query("SELECT * FROM USERS");
+    		$result = $sqlQuery->fetchAll();
+    		echo "Row count: " . count($result) . ".";
+			}
+		catch(Exception $e){
+    		die(var_dump($e));
+			}
+		 ?>
 	</body>
 </html>
