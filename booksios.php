35007ed5502d133044b44fc88372798e9eb17329
Matheus Santos <MatheusTusi@Matheus-Mac-mini.local>
2017-05-01 18:08:01 -0300

    Books Everywhere6

--- booksios.php
+++ booksios.php
@@ -21,7 +21,7 @@
         	
             try {
                	$conexao = Conecta::abrir();
-            	$query = $conexao->prepare("SELECT * FROM BRents ORDER BY RentID");
+            	$query = $conexao->prepare("SELECT * FROM BRents");
                 $query->execute();
                 for($i=0; $row = $query->fetch(); $i++){
                 	
