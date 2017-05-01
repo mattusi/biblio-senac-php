b07af9a2cde51ea496976e58ed42ff6463b33fab
Matheus Santos <MatheusTusi@Matheus-Mac-mini.local>
2017-05-01 05:06:56 -0300

    JSON implemetation4

--- json.php
+++ json.php
@@ -6,15 +6,9 @@
     $query = $conexao->prepare("SELECT * FROM Users ORDER BY UserID");
     $query->execute();
     for($i=0; $row = $query->fetch(); $i++){
-         $temp = array();
- 
-    //inserting the team in the temporary array
-   		$temp['$row[UserID]'] = $team['UserID'];
-    	$temp['$row[UserName]']=$team['UserName'];
-    	$temp['$row[UserPWD]']=$team['UserPWD'];
- 		$temp['$row[UserEmail]']=$team['UserEmail'];
-    //inserting the temporary array inside response
-    array_push($response['user'],$temp);
+        $response[] = $row;
+		 
+   
                            
         }
     echo json_encode($response);    
