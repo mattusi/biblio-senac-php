133d2650c49bdadd54ccf6fc8b9d583f999c70a2
Matheus Santos <MatheusTusi@Matheus-Mac-mini.local>
2017-05-01 13:37:00 -0300

    change of plans login14

--- loginios.php
+++ loginios.php
@@ -25,13 +25,13 @@
             	$query = $conexao->prepare("SELECT UserName, UserID,UserPWD, UserEmail FROM Users ORDER BY UserID");
                 $query->execute();
                 for($i=0; $row = $query->fetch(); $i++){
-                	$tempEmail .= $row[UserEmail];
-                	$tempPWD .= $row[UserPWD];
+                	$tempEmail = $row[UserEmail];
+                	$tempPWD = $row[UserPWD];
                 	$tempID .= $row[UserID];
                 	$tempName .= $row[UserName];
                 	if ($email and $pwd == $tempEmail and $tempPWD){
                 		echo("OK " . $tempID ." ". $tempName . " end");
-                		break;
+                		
                 	}
                 
                 }
