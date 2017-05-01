d6b12240c7d1a3432336d60a9b007ad2db20e48a
Matheus Santos <MatheusTusi@Matheus-Mac-mini.local>
2017-05-01 02:28:31 -0300

    Understanding tables

--- index.php
+++ index.php
@@ -24,12 +24,13 @@
                   <?php
                    include 'conecta.php';
                    $conexao = Conecta::abrir();
-                   $query = $conexao->prepare("SELECT UserID,UserName FROM Users ORDER BY UserName");
+                   $query = $conexao->prepare("SELECT UserID,UserName, UserEmail FROM Users ORDER BY UserName");
                    $query->execute();
                    for($i=0; $row = $query->fetch(); $i++){
                             echo '<tr>';
                             echo '<td>' . $row[UserID] . '</td>';
                             echo '<td>' . $row[UserName] . '</td>';
+                            echo '<td>' . $row[UserEmail] . '</td>'; 
                             echo '<td><a href="leritem.php?id='.$row[UserID].'">Detalhes</a></td>';
                             echo '<td><a href="atualizaritem.php?id='.$row[UserID].'">Atualizar</a></td>';
                             echo '<td><a color=red href="apagaritem.php?id='.$row[UserID].'">Apagar<a></td>';
