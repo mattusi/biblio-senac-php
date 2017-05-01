89e67b62b84400b455b0536ce5c2ea0b9fa01e20
Matheus Santos <MatheusTusi@Matheus-Mac-mini.local>
2017-05-01 17:55:52 -0300

    Books Everywhere

---
+++ booksios.php
@@ -0,0 +1,42 @@
+<?php
+    // Classe de conexao ao DB
+    require 'conecta.php';
+ 	
+    // Se for um post do formulario vamos tratar 
+  
+        // variavel para tratar os erros
+        $nameError = null;
+         
+        // obtem o campo de entrada Nome e atribuii para variavel
+        $userid = ($_POST['userid']);
+        // Valida a entrada
+        $valid = true;
+        if (empty($userid)) {
+            echo('Erro de comunicaçao do POST');
+            $valid = false;
+		}
+         
+        // insert data
+        if ($valid) {
+        	
+            try {
+               	$conexao = Conecta::abrir();
+            	$query = $conexao->prepare("SELECT * FROM BRents ORDER BY RentID");
+                $query->execute();
+                for($i=0; $row = $query->fetch(); $i++){
+                	
+                	if ($userid == $row[UserID]){
+                		echo 'Start:' . $row[BookID] . ":" . $row[RentS] . ":" . $row[RentD];	
+                	
+                }
+            } catch(PDOException $e) {
+				// Se ocorrer erro, apresentar e parar a app 
+				die($e->getMessage()); 
+        	}
+           
+        }
+    
+?>
+
+
+        
